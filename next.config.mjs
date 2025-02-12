7b9a7e9712a5e3489068310fa61d1b1f11c852cd
jamsagency <j@jams.agency>
2025-02-12 18:13:36 -0300

    deploy tweaks

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  reactStrictMode: true,
   images: {
     unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    // Increase the maximum stack size
-    config.optimization.nodeEnv = false;
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        module: false,
-      };
-    }
-    return config;
-  },
-  experimental: {
-    outputFileTracingRoot: process.cwd(),
-  },
+  }
 }
 
 export default nextConfig;
