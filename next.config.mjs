9dd1f17bb743d20d72309e53812028d6ff55e11d
jamsagency <j@jams.agency>
2025-02-12 18:37:08 -0300

    deploys

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  webpack: (config, { isServer, dev }) => {
-    // Disable minimization for easier debugging
-    config.optimization.minimize = false;
-    config.optimization.minimizer = [];
-    
-    return config;
-  },
+  output: 'export',
 }
 
 export default nextConfig;
