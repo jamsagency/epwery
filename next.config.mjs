c919aceb807ec8b8659b248e38aa82fa8b40f031
jamsagency <j@jams.agency>
2025-02-12 18:34:20 -0300

    deploy tweaks

--- next.config.mjs
+++ next.config.mjs
@@ -5,58 +5,12 @@ const nextConfig = {
     unoptimized: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
-  },
-  onDemandEntries: {
-    maxInactiveAge: 25 * 1000,
-    pagesBufferLength: 2,
   },
   webpack: (config, { isServer, dev }) => {
     // Disable minimization for easier debugging
     config.optimization.minimize = false;
     config.optimization.minimizer = [];
-    
-    if (!isServer && !dev) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        minSize: 10000,
-        maxSize: 250000,
-      };
-    }
-
-    // Optimize for large projects
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-    
-    config.optimization.splitChunks = {
-      chunks: 'all',
-      cacheGroups: {
-        default: false,
-        vendors: false,
-        commons: {
-          name: 'commons',
-          chunks: 'all',
-          minChunks: 2,
-          reuseExistingChunk: true,
-        },
-        lib: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module, chunks, cacheGroupKey) {
-            const moduleFileName = module.identifier().split('/').reduceRight(item => item);
-            const allChunksNames = chunks.map((item) => item.name).join('~');
-            return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
-          },
-          chunks: 'all',
-          priority: 1,
-        },
-      },
-    }
     
     return config;
   },
