03c69b3bfde95d9be1032cae4d29410cf82a9fab
jamsagency <j@jams.agency>
2025-02-12 18:09:04 -0300

    deploy tweaks

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,21 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Increase the maximum stack size
+    config.optimization.nodeEnv = false;
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        module: false,
+      };
+    }
+    return config;
+  },
+  experimental: {
+    outputFileTracingRoot: process.cwd(),
+  },
 }
 
 export default nextConfig;
