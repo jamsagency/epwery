190acb2237d696052696b4e5741ac49f496ecbf8
jamsagency <j@jams.agency>
2025-02-12 18:15:21 -0300

    deploy tweaks

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  }
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
 }
 
 export default nextConfig;
